7f9acc7f56f3a37dfd00881d80d54b46fadd0870
Moritisimor <devMoritisimor@proton.me>
2026-07-10 22:44:50 +0200

    Moved runrepl, runscript and stdlibsetup to pkg

--- cmd/saturn/main.go
+++ cmd/saturn/main.go
@@ -4,19 +4,19 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/Moritisimor/SaturnJS/internal"
+	"github.com/Moritisimor/SaturnJS/pkg"
 	"github.com/dop251/goja"
 )
 
 func main() {
 	js := goja.New()
-	if err := internal.StdlibSetup(js); err != nil {
+	if err := pkg.StdlibSetup(js); err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 
 	if len(os.Args) < 2 {
-		if err := internal.RunRepl(js); err != nil {
+		if err := pkg.RunRepl(js); err != nil {
 			fmt.Println(err.Error())
 			os.Exit(1)
 		}
@@ -24,7 +24,7 @@ func main() {
 		return
 	}
 
-	if err := internal.RunScript(js, os.Args[1]); err != nil {
+	if err := pkg.RunScript(js, os.Args[1]); err != nil {
 		fmt.Println(err.Error())
 	}
 }
